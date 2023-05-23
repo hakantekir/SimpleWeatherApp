7a8105fd0a472249d5904c557307a7e4c6569fe0
Hakan <hakantekir2002@outlook.com>
2023-05-23 12:59:28 +0300

    Initial Commit

---
+++ SimpleWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by Hakan Tekir on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
